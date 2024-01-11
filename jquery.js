1ef4491fe9d3987a20a180c2334220010d40e309
kaysuk <99124414+kaysuk@users.noreply.github.com>
2024-01-11 19:23:57 +0300

    карусель, навигация, слайдер, "FAQ"

---
+++ jquery.js
@@ -0,0 +1,59 @@
+$("#center_01").slick({
+  centerMode: true,
+  infinite: true,
+  centerPadding: "10px",
+  slidesToShow: 12,
+  speed: 1600,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "10px",
+        slidesToShow: 12
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "10px",
+        slidesToShow: 12
+      }
+    }
+  ]
+});
+
+$("#center_02").slick({
+  centerMode: true,
+  infinite: true,
+  centerPadding: "10px",
+  slidesToShow: 12,
+  speed: 1600,
+  autoplay: true,
+  autoplaySpeed: 2300,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "10px",
+        slidesToShow: 12
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "10px",
+        slidesToShow: 12
+      }
+    }
+  ]
+});
