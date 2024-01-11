ad3ae6abba13b6bf1b7b9fbf8ec24f9ac0e135a0
kaysuk <99124414+kaysuk@users.noreply.github.com>
2024-01-11 19:32:02 +0300

    корректировки slider.js

---
+++ slider.js
@@ -0,0 +1,46 @@
+let texts = [
+    "Долгие поиски единственного и неповторимого мастера на многострадальный сайт www.cielparfum.com, который был собран крайне некомпетентным программистом и раз в месяц стабильно грозил погибнуть, привели меня на сайт и, в итоге, к ребятам из Drupal-coder. И вот уже практически полгода как не проходит и дня, чтобы я не поудивлялась и не порадовалась своему везению! Починили все, что не работало - от поиска до отображения меню. Провели редизайн - не отходя от желаемого, но со своими существенными и качественными дополнениями. Осуществили ряд проектов - конкурсы, тесты и тд. А уж мелких починок и доработок - не счесть! И главное - все качественно и быстро (не взирая на не самый \"быстрый\" тариф). Есть вопросы - замечательный Алексей всегда подскажет, поддержит, отремонтирует и/или просто сделает с нуля. Есть задумка для реализации - замечательный Сергей обсудит и предложит идеальный вариант. Есть проблема - замечательные Надежда и Роман починят, поправят, сделают! Ребята доказали, что эта CMS - мощная и грамотная система управления. Надеюсь, что наше сотрудничество затянется надолго! Спасибо!!!",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc faucibus a pellentesque sit amet porttitor eget dolor. Venenatis lectus magna fringilla urna. At volutpat diam ut venenatis. Vel pretium lectus quam id leo in. Nulla pharetra diam sit amet nisl suscipit. Duis ut diam quam nulla. Aliquam id diam maecenas ultricies. Ultrices mi tempus imperdiet nulla malesuada pellentesque. Ipsum consequat nisl vel pretium lectus quam id leo. Lectus arcu bibendum at varius vel pharetra. At risus viverra adipiscing at.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc faucibus a pellentesque sit amet porttitor eget dolor. Venenatis lectus magna fringilla urna. At volutpat diam ut venenatis. Vel pretium lectus quam id leo in. Nulla pharetra diam sit amet nisl suscipit. Duis ut diam quam nulla. Aliquam id diam maecenas ultricies. Ultrices mi tempus imperdiet nulla malesuada pellentesque. Ipsum consequat nisl vel pretium lectus quam id leo. Lectus arcu bibendum at varius vel pharetra. At risus viverra adipiscing at.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc faucibus a pellentesque sit amet porttitor eget dolor. Venenatis lectus magna fringilla urna. At volutpat diam ut venenatis. Vel pretium lectus quam id leo in. Nulla pharetra diam sit amet nisl suscipit. Duis ut diam quam nulla. Aliquam id diam maecenas ultricies. Ultrices mi tempus imperdiet nulla malesuada pellentesque. Ipsum consequat nisl vel pretium lectus quam id leo. Lectus arcu bibendum at varius vel pharetra. At risus viverra adipiscing at. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc faucibus a pellentesque sit amet porttitor eget dolor. Venenatis lectus magna fringilla urna. At volutpat diam ut venenatis. Vel pretium lectus quam id leo in. Nulla pharetra diam sit amet nisl suscipit. Duis ut diam quam nulla. Aliquam id diam maecenas ultricies. Ultrices mi tempus imperdiet nulla malesuada pellentesque. Ipsum consequat nisl vel pretium lectus quam id leo. Lectus arcu bibendum at varius vel pharetra. At risus viverra adipiscing at",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc faucibus a pellentesque sit amet porttitor eget dolor. Venenatis lectus magna fringilla urna. At volutpat diam ut venenatis. Vel pretium lectus quam id leo in. Nulla pharetra diam sit amet nisl suscipit. Duis ut diam quam nulla. Aliquam id diam maecenas ultricies. Ultrices mi tempus imperdiet nulla malesuada pellentesque. Ipsum consequat nisl vel pretium lectus quam id leo. Lectus arcu bibendum at varius vel pharetra. At risus viverra adipiscing atsed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc faucibus a pellentesque sit amet porttitor eget dolor. Venenatis lectus magna fringilla urna. At volutpat diam ut venenatis. Vel pretium lectus quam id leo in. Nulla pharetra diam sit amet nisl suscipit. Duis ut diam quam nulla. Aliquam id diam maecenas ultricies. Ultrices mi tempus imperdiet nulla malesuada pellentesque. Ipsum consequat nisl vel pretium lectus.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc faucibus a pellentesque sit amet porttitor eget dolor. Venenatis lectus magna fringilla urna. At volutpat diam ut venenatis. Vel pretium lectus quam id leo in. Nulla pharetra diam sit amet nisl suscipit. Duis ut diam quam nulla. Aliquam id diam maecenas ultricies. Ultrices mi tempus imperdiet nulla malesuada pellentesque. Ipsum consequat nisl vel pretium lectus quam id leo. Lectus arcu bibendum at varius vel pharetra. At risus viverra adipiscing at.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc faucibus a pellentesque sit amet porttitor eget dolor. Venenatis lectus magna fringilla urna. At volutpat diam ut venenatis. Vel pretium lectus quam id leo in. Nulla pharetra diam sit amet nisl suscipit.  nisl suscipit. Duis ut diam quam nulla. Aliquam id diam maecenas Duis ut diam quam nulla. Aliquam id diam maecenas ultricies. Ultrices mi tempus imperdiet nulla malesuada pellentesque. Ipsum consequat nisl vel pretium lectus quam id leo. Lectus arcu bibendum at varius vel pharetra. At risus viverra adipiscing at.",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc faucibus a pellentesque sit amet porttitor eget dolor. Venenatis lectus magna fringilla urna. At volutpat diam ut venenatis. Vel pretium lectus quam id leo in. Nulla pharetra diam sit amet nisl suscipit. Duis ut diam quam nulla. Aliquam id diam maecenas ultricies. Ultrices mi tempus imperdiet nulla malesuada pellentesque. Ipsum consequat nisl vel pretium lectus quam id leo. Lectus arcu bibendum at varius vel pharetra. At risus viverra adipiscing at.",
+    
+]
+
+
+
+let i = 0;
+function showComment(k)
+{
+    document.getElementById("comment").innerHTML = texts[i];
+}
+
+
+
+
+document.getElementById("slider__next").onclick = function() 
+{
+    
+    i++;
+    if(i <8) {}
+    else i= 0;
+    document.getElementById("slider__position").innerHTML = "0" + parseInt(i + 1) ;
+    
+    showComment(i);
+}
+
+document.getElementById("slider__prev").onclick = function() 
+{
+    
+    i--;
+    if(i == -1) {
+        i = 7;
+    }
+    document.getElementById("slider__position").innerHTML = "0" + parseInt(i + 1) ;
+    
+    showComment(i);
+
+}
