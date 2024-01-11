30fa6bfb96e664b184cb2f045955ab126f405515
kaysuk <99124414+kaysuk@users.noreply.github.com>
2024-01-11 18:56:11 +0300

    Delete 210group directory

--- 210group/main.js
+++
@@ -1,5 +0,0 @@
-// переключатель состояния меню
-// document.querySelector(".hamburger-menu").addEventListener("click", () => {
-//     document.querySelector(".nav-links").classList.toggle("show-menu");
-//     document.querySelector(".hamburger-menu").classList.toggle("show-menu");
-// });
