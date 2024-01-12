8c5f887badac7985e4ecc0fd9ecf61343ec706de
Fedor Zavgorodniy <144099697+Zavgar69@users.noreply.github.com>
2024-01-12 09:20:52 +0300

    Add files via upload

--- jquery.js
+++ jquery.js
@@ -1,4 +1,68 @@
+
+
+if (document.documentElement.clientWidth < 777) {
+
 $("#center_01").slick({
+  centerMode: true,
+  infinite: true,
+  centerPadding: "10px",
+  slidesToShow: 6,
+  speed: 1600,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "10px",
+        slidesToShow: 6 // 12
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "10px",
+        slidesToShow: 6
+      }
+    }
+  ]
+});
+
+$("#center_02").slick({
+  centerMode: true,
+  infinite: true,
+  centerPadding: "10px",
+  slidesToShow: 6,
+  speed: 1600,
+  autoplay: true,
+  autoplaySpeed: 2300,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "10px",
+        slidesToShow: 6
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "10px",
+        slidesToShow: 6
+      }
+    }
+  ]
+});
+} else {
+  $("#center_01").slick({
   centerMode: true,
   infinite: true,
   centerPadding: "10px",
@@ -13,7 +77,7 @@ $("#center_01").slick({
         arrows: false,
         centerMode: true,
         centerPadding: "10px",
-        slidesToShow: 12
+        slidesToShow: 12 // 12
       }
     },
     {
@@ -57,3 +121,4 @@ $("#center_02").slick({
     }
   ]
 });
+}
